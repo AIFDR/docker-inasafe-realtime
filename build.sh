eb6edfb48a2db916acb0ceb2e43976d2f910c9ac
Akbar Gumbira <akbargumbira@gmail.com>
2014-08-12 10:12:44 +0700

    Use lowercase for image name and use docker instead of docker.io

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ INASAFE_REALTIME_IMAGE=docker-realtime-inasafe
 
 function build_realtime_image {
     echo "Building InaSAFE Realtime Dockerfile"
-    docker.io build -t AIFDR/${INASAFE_REALTIME_IMAGE} .
+    docker build -t aifdr/${INASAFE_REALTIME_IMAGE} .
 }
 
 build_realtime_image
